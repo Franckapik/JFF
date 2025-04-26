26d6a90e70bee41618d7dc5854cf6137cde17278
Franckapik <fanch44@hotmail.com>
2025-04-26 15:39:42 +0200

    rename

---
+++ src/managers/BotManager.js
@@ -0,0 +1,42 @@
+import useBotStore from "../stores/useBotStore";
+import useTileStore from "../stores/useNewTileStore";
+import usePlayerStore from "../stores/usePlayerStore";
+
+const BotManager = () => {
+  const { state, updateState, execute } = useBotStore();
+  const tiles = useTileStore((state) => state.tiles);
+  const updateShip = usePlayerStore((state) => state.updateShip);
+
+  const performAction = () => {
+    // Execute the current state's action
+    execute(tiles);
+
+    const botTargetTile = useBotStore.getState().targetTile;
+    if (botTargetTile) {
+      const targetTile = tiles[botTargetTile];
+
+      if (targetTile) {
+        // Example: Update player2's ship position to move toward the target
+        updateShip("player2", {
+          coord: botTargetTile,
+          position: targetTile.position,
+        });
+
+        // Transition to the next state based on the tile type
+        if (targetTile.type === "fuel") {
+          updateState("refueling");
+        } else if (targetTile.type === "repair") {
+          updateState("repairing");
+        } else if (targetTile.resources) {
+          updateState("exploring");
+        } else {
+          updateState("idle");
+        }
+      }
+    }
+  };
+
+  return { performAction };
+};
+
+export default BotManager;
