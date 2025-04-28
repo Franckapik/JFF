0a4604ee669e58dd937e6c144f3748539ffda1ba
Franckapik <fanch44@hotmail.com>
2025-04-28 16:01:07 +0200

    too much removed in tile store

--- src/stores/useNewTileStore.js
+++ src/stores/useNewTileStore.js
@@ -3,9 +3,28 @@ import { generateHexPositions } from '../utils/utils'; // Import the utility fun
 
 export const useTileStore = create((set, get) => ({
     tiles: {},
+    selectedTile: null,
     radius: 3, // Default radius
     spacing: 0.1, // Default spacing
     setTiles: (tiles) => set({ tiles }),
+    clearSelectedTile: () => {
+        console.log("Clearing selected tile"); // Log the action
+        set({ selectedTile: null });
+    },
+    getTile: (coord) => get().tiles[coord],
+    getNeighbors: (coord) => {
+        const tile = get().tiles[coord];
+        return tile ? tile.neighbors.map((neighbor) => get().tiles[neighbor]) : [];
+    },
+    updateTile: (coord, updates) => {
+        set((state) => {
+            const updatedTiles = { ...state.tiles };
+            if (updatedTiles[coord]) {
+                updatedTiles[coord] = { ...updatedTiles[coord], ...updates };
+            }
+            return { tiles: updatedTiles };
+        });
+    },
     clearTiles: () => set({ tiles: {} }),
 
     // Initialize tiles using radius and spacing from the store
