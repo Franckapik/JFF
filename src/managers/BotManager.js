6181ce3720a45a2812291bc24040c88e3761f12c
Franckapik <fanch44@hotmail.com>
2025-04-29 14:41:38 +0200

    delete old files

--- src/managers/BotManager.js
+++
@@ -1,3 +0,0 @@
-// This file is now obsolete and should be removed.
-// The functionality has been migrated to the useBotStore.js
-// Use useBotStore.getState().processBotTurn() instead of BotManager().performAction()
